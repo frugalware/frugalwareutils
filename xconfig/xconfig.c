7af7a2ba9b6f6978412fe893acb8c908f97c5b87
VMiklos <vmiklos@frugalware.org>
2006-05-26 00:32:38 +0000

    xconfig: clean up includes

--- xconfig/xconfig.c
+++ xconfig/xconfig.c
@@ -25,11 +25,8 @@
 #include <libfwutil.h>
 #include <libfwxconfig.h>
 #include <setup.h>
-#include <stdlib.h>
-#include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <libintl.h>
 
 int run()
