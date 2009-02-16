6d9451c7126b43c6cc51b7b7b8043847dd9f9582
Miklos Vajna <vmiklos@frugalware.org>
2009-02-16 23:21:55 +0100

    updates for 0.8.6

--- libfwnetconfig/libfwnetconfig.c
+++ libfwnetconfig/libfwnetconfig.c
@@ -3,7 +3,7 @@
  *
  *  useful functions for network configuration
  * 
- *  Copyright (c) 2006, 2008 by Miklos Vajna <vmiklos@frugalware.org>
+ *  Copyright (c) 2006, 2008, 2009 by Miklos Vajna <vmiklos@frugalware.org>
  * 
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
