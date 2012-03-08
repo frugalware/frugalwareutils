4393ea2a3e3b55b90a5bae772d1d046db78062cf
James Buren <ryuo@frugalware.org>
2012-03-08 03:46:39 -0600

    remove outdated code using /etc/hardwareclock rewrite API to call hwclock directly to setup its config file in UTC or localtime (/etc/adjtime)

--- libfwtimeconfig/libfwtimeconfig.h
+++ libfwtimeconfig/libfwtimeconfig.h
@@ -1,8 +1,8 @@
 /*
  *  libfwtimeconfig.h for frugalwareutils
- * 
+ *
  *  Copyright (c) 2006 by Miklos Vajna <vmiklos@frugalware.org>
- * 
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
@@ -15,9 +15,9 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
  *  USA.
  */
 
-int fwtime_hwclockconf(char *path, char *mode);
+int fwtime_hwclockconf(char *mode);
 int fwtime_find(char *dirname);
